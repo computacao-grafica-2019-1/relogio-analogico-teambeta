3d0391799bc7d3b905a968ddc356d0f1e7ca0427
Lucas Anselmo <30813248+lucas62@users.noreply.github.com>
2019-02-22 18:06:03 -0300

    Atualização do código com hora atualizada.

--- sketch.js
+++ sketch.js
@@ -5,9 +5,7 @@ var raioDosNumeros = 100;
 var cx, cy;
 var px, py;
 var destaque_theta;
-var hora = 3;
-var nl =0;
-
+var hora = 1;
 
 function setup() {
  createCanvas(400, 400);
@@ -15,6 +13,7 @@ function setup() {
  // centro do desenho e do circulo
  cx = width / 2;
  cy = height / 2;
+	
  // ângulo theta para arco de destaque
  // que é o mesmo theta para o ponto p
  destaque_theta = PI * 1,75;
@@ -38,20 +37,15 @@ function setup() {
   
  pppx += cx;
  pppy += cy;
-	
-	noLoop();
   
 }
 
 function draw() {
 	
- background(250);
+ background(220);
  noFill();
  stroke(1);
-	strokeWeight(4);
-	fill(150, 150, 150);
  ellipse(cx, cy, 350);
-	fill(255);
  ellipse(cx, cy, 305);
  fill(255);
  for (var angulo = 0; angulo <= 360; angulo += 30) {
@@ -71,17 +65,10 @@ function draw() {
 	 line(x + cx, y + cy, xx+ cx, yy+cy);
    fill(0);
    textSize(40);
-
+   textStyle(BOLD);
    text(hora, xxx + cx, yyy + cy);
 	 noFill();
-	 	if (hora<12){
-			hora++;
-			}
-	 else{
-	 	hora=1;
-	 }
  }
-	
  	// desenha uma elipse na coord (cx,cy)
   strokeWeight(1);
   fill(0);
@@ -91,14 +78,11 @@ function draw() {
   
   stroke(1);
   strokeWeight(5);
-	stroke('red');
   line(cx, cy, cx + raioDosNumeros - 45, cy);
-	stroke('black');
-  line(cx, cy, pppx+30, pppy);  
+  line(cx, cy, pppx + 15, pppy);  
   
   noStroke();
-  fill(245,0,0);
+  fill(0);
   triangle(cx + raioDosNumeros - 30, cy, 250, cy + 10, 250, cy - 10);
-	fill(0);
- 	triangle(cx - raioDosNumeros + 10, cy, 130, cy + 10, 130, cy - 10);
+  triangle(cx - raioDosNumeros, cy, 120, cy + 10, 120, cy - 10);
 }
